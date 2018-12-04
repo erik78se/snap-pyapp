2674087477d43251b22e7ebb1b6b6edbad80632f
erik78se <erik.lonroth@gmail.com>
2018-12-04 18:32:31 +0100

    initial

---
+++ README-building.txt
@@ -0,0 +1,7 @@
+Missing python3 pip:
+   https://forum.snapcraft.io/t/no-module-named-pip/3483
+
+Travis
+https://tutorials.ubuntu.com/tutorial/continuous-snap-delivery-from-travis-ci#0
+
+SNAPCRAFT_BUILD_ENVIRONMENT=host snapcraft
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="myapp",
+    url="http://lonroth.net",
+    author="Me",
+    author_email="erik.lonroth@gmail.com",
+    version="0.1",
+    scripts=['app.py']
+)
